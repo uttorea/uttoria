f88e09398a4a114ddc28c3db46f69fb8c5706d5c
Nikhil Rai <nikhilrai662@gmail.com>
2024-06-02 19:23:41 +0530

    first commit

--- src/components/product3.js
+++ src/components/product3.js
@@ -1,46 +1,9 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import './product3.css';
 
 const Product3 = () => {
   const [zoomLevel, setZoomLevel] = useState(1);
-  const contentRef = useRef(null);
   const sliderRef = useRef(null);
-  const [isVisible, setIsVisible] = useState(false);
-
-  useEffect(() => {
-    if (!('IntersectionObserver' in window)) {
-      import('intersection-observer').then(() => {
-        initIntersectionObserver();
-      });
-    } else {
-      initIntersectionObserver();
-    }
-
-    function initIntersectionObserver() {
-      const observer = new IntersectionObserver(
-        (entries) => {
-          entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-              setIsVisible(true);
-            } else {
-              setIsVisible(false);
-            }
-          });
-        },
-        { threshold: 0.1 }
-      );
-
-      if (contentRef.current) {
-        observer.observe(contentRef.current);
-      }
-
-      return () => {
-        if (contentRef.current) {
-          observer.unobserve(contentRef.current);
-        }
-      };
-    }
-  }, []);
 
   const handleMouseMove = (event) => {
     if (sliderRef.current) {
@@ -68,103 +31,158 @@ const Product3 = () => {
   const translateYValue = (0 + zoomLevel) * 100;
 
   return (
-    <div className="container" ref={contentRef}>
-      <h1 className="ballistic-emergency-recovery-container13">
-        Ballistic Emergency Recovery Systems For Multi-Rotor UAVS
-      </h1>
-      <div
-        className="ellipse"
-        style={{
-          backgroundColor: '#242425',
-        }}
-      >
+    <>
+      <div className="container">
+        <h1 className="ballistic-emergency-recovery-container13">
+          Ballistic Emergency Recovery Systems For Multi-Rotor UAVS
+        </h1>
         <div
-          className="frame-wrapper53"
+          className="ellipse"
           style={{
-            transform: `scale(${1 - zoomLevel})`,
-            display: zoomLevel < 1 ? 'block' : 'none',
-            transition: 'transform 0.5s ease-out',
+            backgroundColor: '#242425',
           }}
         >
-          <div className="frame-parent363">
-            {/* (Same content as before) */}
-          </div>
-        </div>
-      </div>
-      <div className="frame-wrapper63">
-        <div className="arsentm-series-offers-built-in-parent3">
-          <div className="arsentm-series-offers-container3">
-            <b>ARSENTM</b>
-            <span>
-              {" "}
-              Series offers built in intelligence and split-second decision
-              making capable recovery system
-            </span>
-          </div>
-          <div
-            className="frame-wrapper7"
-            style={{
-              transform: `translateY(${translateYValue}px)`,
-              display: translateYValue > 70 ? 'none' : 'flex',
-              transition: 'transform 0.2s',
-              position: 'absolute',
-              marginTop: '70px',
-            }}
-          >
-            <div className="explore-arsentm-series-parent3">
-              <div className="explore-arsentm-series-container3">
-                <span className="explore-arsentm-series-container13">
-                  <span>{`Explore `}</span>
-                  <span className="arsentm3">ArsenTM</span>
-                  <span> Series</span>
-                </span>
+          <div className="frame-wrapper53"
+          style={{
+            transform: `scale(${1 - zoomLevel})`,
+            display: zoomLevel < 1 ? 'block' : 'none'
+          }}>
+            <div className="frame-parent363">
+              <div className="pwm-based-manual-sensor-base-parent3">
+                <div className="pwm-based-manual-container3">
+                  <p className="pwm-based3">PWM based</p>
+                  <p className="manual-sensor3">Manual / Sensor based</p>
+                  <p className="automated-ejection-mod3">
+                    Automated Ejection Mod
+                  </p>
+                </div>
+                <div className="minimum-effective-height-20-wrapper3">
+                  <div className="minimum-effective-height-container3">
+                    <p className="minimum3">Minimum</p>
+                    <p className="effective-height-3">Effective Height - 20 m</p>
+                    <p className="above-ground3">above ground</p>
+                  </div>
+                </div>
+                <div className="descent-speed-5-ms-wrapper3">
+                  <div className="descent-speed-container3">
+                    <p className="descent3">Descent</p>
+                    <p className="speed-53">Speed - 5 m/s</p>
+                  </div>
+                </div>
               </div>
-              <div className="vector-wrapper3">
-                <img className="vector-icon1" alt="" src='/vector1.svg' />
+              <div className="pneumatic-micro-servo-sprin-parent3">
+                <div className="pneumatic-micro-servo-container3">
+                  <p className="pneumatic-micro-servo3">
+                    Pneumatic/ Micro Servo + Spring for
+                  </p>
+                  <p className="ballistic-ejection3">Ballistic Ejection</p>
+                </div>
+                <div className="waterproof-and-dust-protection-wrapper3">
+                  <div className="waterproof-and-dust-container3">
+                    <p className="waterproof-and3">Waterproof and</p>
+                    <p className="dust-protection3">Dust Protection</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="advanced-carbon-composite-stru-parent3">
+                <div className="advanced-carbon-composite-container3">
+                  <p className="advanced-carbon3">Advanced Carbon</p>
+                  <p className="composite-structure3">Composite Structure</p>
+                </div>
+                <div className="operating-temperature-10-to-wrapper3">
+                  <div className="operating-temperature-container3">
+                    <p className="operating-temperature3">
+                      Operating Temperature =
+                    </p>
+                    <p className="to-50-deg3">-10 to 50 deg C</p>
+                  </div>
+                </div>
+                <div className="g-shock-on-mounting-points-wrapper3">
+                  <div className="g-shock-on-container3">
+                    <p className="g-shock-on3">3G Shock on</p>
+                    <p className="mounting-points3">Mounting Points</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div className="frame-wrapper63">
+          <div className="arsentm-series-offers-built-in-parent3">
+            <div className="arsentm-series-offers-container3">
+              <b>ARSENTM</b>
+              <span>
+                {" "}
+                Series offers built in intelligence and split-second decision
+                making capable recovery system
+              </span>
+            </div>
+            <div
+              className="frame-wrapper7"
+              style={{
+                transform: `translateY(${translateYValue}px)`,
+                display: translateYValue > 70 ? 'none' : 'flex',
+                transition: 'transform 0.2s',
+                position: 'absolute',
+                marginTop: '70px'
+              }}
+            >
+              <div className="explore-arsentm-series-parent3">
+                <div className="explore-arsentm-series-container3">
+                  <span className="explore-arsentm-series-container13">
+                    <span>{`Explore `}</span>
+                    <span className="arsentm3">ArsenTM</span>
+                    <span> Series</span>
+                  </span>
+                </div>
+                <div className="vector-wrapper3">
+                  <img className="vector-icon1" alt="" src='/vector1.svg' />
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div
-        className="ellipse"
-        style={{
-          transform: `scale(${zoomLevel})`,
-          backgroundColor: '#13131A',
-          display: zoomLevel >= 0 ? 'block' : 'none',
-        }}
-      ></div>
-      <img
-        src='/image-54@2x.png'
-        alt="Overlay"
-        className="overlay-image"
-        style={{
-          transform: `scale(${0.7 + 0.3 * zoomLevel})`,
-          transition: 'transform 0.5s ease-out',
-        }}
-      />
-      <div
-        ref={sliderRef}
-        className="zoom-controls"
-        onMouseMove={handleMouseMove}
-        onTouchMove={handleTouchMove}
-        style={{ right: '50px', top: '40%', height: '150px', width: '50px' }}
-      >
         <div
-          className="zoom-handle"
+          className="ellipse"
           style={{
-            position: 'absolute',
-            left: '50%',
-            transform: 'translateX(-50%)',
-            bottom: `${zoomLevel * 100}%`,
-            width: '45.41px',
-            height: '192.74px',
-            background: 'white',
-            cursor: 'pointer',
+            transform: `scale(${zoomLevel})`,
+            backgroundColor: '#13131A',
+            display: zoomLevel >= 0 ? 'block' : 'none'
           }}
         ></div>
+        <img
+          src='/image-54@2x.png'
+          alt="Overlay"
+          className="overlay-image"
+          style={{
+            transform: `scale(${0.7 + 0.3 * zoomLevel})`
+          }}
+        />
+        <div
+          ref={sliderRef}
+          className="zoom-controls"
+          onMouseMove={handleMouseMove}
+          onTouchMove={handleTouchMove}
+          style={{ right: '50px', top: '40%', height: '150px', width: '50px', }}
+        >
+          <div
+            className="zoom-handle"
+            style={{
+              position: 'absolute',
+              left: '50%',
+              transform: 'translateX(-50%)',
+              bottom: `${zoomLevel * 100}%`,
+              width: '45.41px',
+              height: '192.74px',
+              background: 'white',
+              cursor: 'pointer'
+            }}
+          ></div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
